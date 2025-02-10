2eba42ca6a4f3e42dea4d532c76a0eb6cbc3d54c
Nickkrylev <mikita.krylev@gmail.com>
2025-02-10 14:58:32 +0200

    Fix: removed database module , added type orm

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -1,18 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DatabaseModule } from './database/database.module';
 import { UsersModule } from './users/users.module';
 import { FocusModule } from './focus/focus.module';
 import { NotificationsModule } from './notifications/notifications.module';
 import { TasksModule } from './tasks/tasks.module';
-import { TasksModule } from './tasks/tasks.module';
-import { NotificationsModule } from './notifications/notifications.module';
-import { FocusModule } from './focus/focus.module';
-import { UsersModule } from './users/users.module';
+
 
 @Module({
-  imports: [DatabaseModule, UsersModule, TasksModule, NotificationsModule, FocusModule],
+  imports: [ UsersModule, TasksModule, NotificationsModule, FocusModule],
   controllers: [AppController],
   providers: [AppService],
 })
